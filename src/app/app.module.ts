83b235eeb3d3bdf3d2100520324d8d11fa17b9ff
Vivek <vivek@viveks-air.home>
2018-04-08 14:09:02 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { MemberComponent } from './member/member.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    MemberComponent
   ],
   imports: [
     BrowserModule
+     RouterModule.forRoot( [
+       {
+          path: 'Member',
+         component: MemberComponent
+       }
+       ,
+       {
+        path: 'Product',
+        component: ProductComponent
+      }
+     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
